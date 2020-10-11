e1588772c9622643b95b9e3e56195c03a6d32438
umedith <umedith03@gmail.com>
2020-10-11 16:22:56 +0200

    add a detailed readme file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,51 @@
 $(document).ready(function() {
     //BUSINESS LOGIC
-    function placeOrder(size, crust, toppings) {
+    function placeOrder(size, crust, topping, numberofpizzas) {
         this.sizeChoice = size;
         this.sizeCrust = crust;
-        this.sizeTopping = toppings;
+        this.sizeTopping = topping;
+        this.sizeNumberOfPizzas = numberofpizzas;
 
 
     }
     placeOrder.prototype.summary = function() {
 
-        return "pizzasize: " + this.sizeChoice + ", crust:" + this.sizeCrust + ", toppings:" + this.sizeTopping;
+        return "pizzasize: " + this.sizeChoice + ", crust:" + this.sizeCrust + ", topping:" + this.sizeTopping + ", numberofpizzas:" +this.sizeNumberOFPizzas;
     };
+
+    
+
+      //USER INTERFACE
+      $("#submit").click(function() {
+        var size = $('#size').val();
+        var crust = $('#crust').val();
+        var numberofpizzas = $("#numberofpizzas").val();
+        var topping = [];
+        $("input[type=checkbox]:checked").each(function() {
+            topping.push($(this).val());
+        });
+        console.log(topping);
+
+        var newOrder = new placeOrder(size, crust, topping, numberofpizzas);
+        console.log(newOrder);
+        $('ul#placeorderlist').append("<li>" + newOrder.summary() + "</li>");
+
+        var anotherTopping = 0;
+        for (var a = 0; a < topping.length; a++) {
+            anotherTopping += parseInt(topping[a]);
+        }
+
+        var total = parseInt(size) + parseInt(crust) + parseInt(anotherTopping) + parseInt(numberofpizzas);
+        console.log(total);
+        $("#total").text(total);
+    });
+
+    $('#delivery').click(function() {
+
+        alert("the delivery cost is 2000");
+        var a = prompt("enter ur location");
+        alert("your order will be delivered to" + " " + a);
+    });
+
+
+});
---
+++ readme.md
@@ -0,0 +1,38 @@
+# The Pizz-Eria
+
+done by Edith Reine
+
+## Description
+This is an online application selling pizza , whereby customers can order any kind of pizza they want to eat depending on their taste and pockets . and we are always pleased to take  your orders and deliver it on time.
+
+## Setup/Installation Requirements
+
+* firstly if you want to work with us you can visit our live website
+
+* Then our page can be found on github
+
+* clone it to your desktop
+
+* you will have the access every single layout
+
+* you can edit or do anything with it after.
+
+
+## Technologies Used
+This project was built under the following program languages:
+
+* html to  display the content
+* css/bootstrap for styling our page and for it to be customised
+* javascript/jquerries to interact with users and for it to be customised as well
+
+## Support and contact details
+May you have any concerns, comments, questions or any other thing, please do not hesitate to reach out to me:
+* email: umedith03@gmail.com
+* github: umedith
+* instagram: umedith
+* contact: +250781703331
+
+### License
+*Copyright (c) {2020} by Edith
+
+
